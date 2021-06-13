da9dcf185e638b99dec17d1b6ee3349a45b5d045
mixass <67761405+mixass@users.noreply.github.com>
2021-06-13 15:44:21 +0300

    Add files via upload

---
+++ tr-tutorial.sh
@@ -0,0 +1,18 @@
+# Komutlar
+ls # Bulundugunuz directoryde olan dosyalari listeler
+cd # Dosyalar arasindaki gecisi saglar ornek cd Desktop
+cd .. # Ust dosyaya gecer
+rmdir # Klasor siler Ornek kullanim : rmdir test4 (Burada test4 silinmesini istedigimiz klasor) ps : sileceginiz dosyanin konumuna gitmeniz gerekiyor (via cd)
+rm # 'Dosya' silmek icin kullanilir. 
+exit # Bulunan shell i kapatir.
+touch # Bulundugunuz konumda dosya olusturur Ornek kullanim : touch mixass.txt
+history # Shellde kullandiginiz komutlari listeler/siralar.
+cat # Dosya icerisini okumaniza yariyor. Ornek kullanim : cat mixass.txt (Dosyanin tamamini printler)
+echo # Print icin kullanilir. '' icerisinde text yazmak okunurlugu artirir
+mkdir # Klasor olusturmanizi saglar Ornek kullanim : mkdir MIXAS
+cp # Dosya - Klasor kopyalanmasi icin kullanilir.
+clear # Shell i temizler // Kisayolu ctrl l
+who # Shell e baglanti kuran kisileri listeler # Remote connectionlarda ise yarar
+pwd # Bulundugunuz konumun bilgisini verir
+date # Tarih
+
