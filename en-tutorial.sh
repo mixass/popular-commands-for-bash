142299de661b407061c7234e88fb24e74837379e
mixass <67761405+mixass@users.noreply.github.com>
2021-06-13 15:54:56 +0300

    Add files via upload

---
+++ en-tutorial.sh
@@ -0,0 +1,17 @@
+# Commands
+ls # List the files in the current folder
+cd # Allows switching between files Example use : cd Desktop
+cd .. # Moves to up directory
+rmdir # Deletes folder Example use : rmdir test4 (i want to delete test4) ps : You should go to that folder  (via cd)
+rm # Used to delete "files". 
+exit # closes the shell.
+touch # Creates a file at your current location Example Use : touch mixass.txt
+history # Lists history commands.
+cat # It helps you to read the file. Example use : cat mixass.txt (Prints all the file)
+echo #Use for print.
+mkdir # You to create a folder Example use : mkdir MIXAS
+cp # File - Folder used to copy.
+clear # cleans the shell // Shortcut CTRL + L
+who # Lists users who have connected to Shell
+pwd # Gives you the information of your current folder
+date # Date xd
